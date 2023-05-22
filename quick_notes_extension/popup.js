f392e874d5900c8cfef7cd59f62b5b721acac3df
roberto.perez <roberto.perez@epicor.com>
2023-05-21 23:58:38 -0600

    updates

--- quick_notes_extension/popup.js
+++ quick_notes_extension/popup.js
@@ -1,24 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var noteInput = document.getElementById('note-input');
-  
-    // Guardar notas al escribir
-    noteInput.addEventListener('input', function() {
-      saveNote();
-    });
-  
-    // Cargar notas guardadas al cargar la extensión
-    loadNote();
-  
-    function saveNote() {
-      chrome.storage.sync.set({ 'note': noteInput.value }, function() {
-        console.log('Nota guardada');
-      });
-    }
-  
-    function loadNote() {
-      chrome.storage.sync.get('note', function(data) {
-        noteInput.value = data.note || '';
-      });
-    }
-  });
-  
+// Function to save a new note
+function saveNote() {
+  var noteInput = document.getElementById("note-input");
+  var noteText = noteInput.value;
+  if (noteText !== "") {
+    var noteList = document.getElementById("note-list");
+    var noteItem = document.createElement("li");
+    noteItem.appendChild(document.createTextNode(noteText));
+    noteList.appendChild(noteItem);
+    noteInput.value = "";
+  }
+}
+
+// Function to add a new task
+function addTask() {
+  var taskInput = document.getElementById("task-input");
+  var taskText = taskInput.value;
+  if (taskText !== "") {
+    var taskList = document.getElementById("task-list");
+    var taskItem = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    taskItem.appendChild(checkbox);
+
+    var label = document.createElement("label");
+    label.textContent = taskText; // Agrega el contenido del input como texto del label
+    taskItem.appendChild(label);
+
+    taskList.appendChild(taskItem);
+
+    taskInput.value = "";
+  }
+}
+
+
+function openTab(tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tabName).style.display = "block";
+  document.getElementById(tabName + "-link").classList.add("active");
+
+  if (tabName === "notesTab") {
+    var noteList = document.getElementById("note-list");
+    noteList.style.display = "block";
+  } else {
+    var taskList = document.getElementById("task-list");
+    taskList.style.display = "block";
+  }
+}
+
